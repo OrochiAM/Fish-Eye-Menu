dc77ece9213cc8c93f364fdc8e8ec6a60b15cb6b
Aleksa Milic <baba12milic@gmail.com>
2026-02-06 19:05:34 +0100

    Hover Single Item.

--- js/main.js
+++ js/main.js
@@ -2,12 +2,21 @@ const list = document.querySelector('ul');
 
 let itemNum = Math.floor(window.innerHeight / 20);
 
-let s = '';
-
 for (let i = 0; i < itemNum; i++) {
-  s += `
-    <li>Item ${i}</li>
-  `;
-}
+  const li = document.createElement('li');
 
-list.innerHTML = s;
+  li.innerHTML = `Item #${i}`;
+
+  li.addEventListener('mouseenter', function (e) {
+    e.target.classList.toggle('class-1');
+    console.log(e.target.className);
+  });
+
+  li.addEventListener('mouseleave', function (e) {
+    e.target.className = 'class-1';
+    e.target.classList.toggle('class-1');
+    console.log(e.target.className);
+  });
+
+  list.appendChild(li);
+}
