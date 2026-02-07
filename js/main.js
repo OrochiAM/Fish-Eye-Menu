6c8e0c8e1574f684ca72f3f274a325698929f3cd
Aleksa Milic <baba12milic@gmail.com>
2026-02-07 12:08:58 +0100

    Finished.

--- js/main.js
+++ js/main.js
@@ -10,11 +10,52 @@ for (let i = 0; i < itemNum; i++) {
   li.addEventListener('mouseenter', function (e) {
     e.target.classList.toggle('class-1');
     console.log(e.target.className);
+
+    if (e.target.previousElementSibling) {
+      e.target.previousElementSibling.classList.toggle('class-2');
+
+      if (e.target.previousElementSibling.previousElementSibling) {
+        e.target.previousElementSibling.previousElementSibling.classList.toggle(
+          'class-3',
+        );
+      }
+    }
+
+    if (e.target.nextElementSibling) {
+      e.target.nextElementSibling.classList.toggle('class-2');
+
+      if (e.target.nextElementSibling.nextElementSibling) {
+        e.target.nextElementSibling.nextElementSibling.classList.toggle(
+          'class-3',
+        );
+      }
+    }
   });
 
   li.addEventListener('mouseleave', function (e) {
     e.target.className = 'class-1';
     e.target.classList.toggle('class-1');
+
+    if (e.target.previousElementSibling) {
+      e.target.previousElementSibling.classList.toggle('class-2');
+
+      if (e.target.previousElementSibling.previousElementSibling) {
+        e.target.previousElementSibling.previousElementSibling.classList.toggle(
+          'class-3',
+        );
+      }
+    }
+
+    if (e.target.nextElementSibling) {
+      e.target.nextElementSibling.classList.toggle('class-2');
+
+      if (e.target.nextElementSibling.nextElementSibling) {
+        e.target.nextElementSibling.nextElementSibling.classList.toggle(
+          'class-3',
+        );
+      }
+    }
+
     console.log(e.target.className);
   });
 
